6a1250973e7ef5009dc1f1b0f24a68b0d559ef22
Yeonji Baek <yeonji815@gmail.com>
2021-07-20 12:00:41 -0700

    Initial Commit

---
+++ KWK_Project3/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  KWK_Project3
+//
+//  Created by Yeonji Baek on 7/19/21.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ KWK_Project3/ToDoTableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ToDoTableViewController.swift
+//  KWK_Project3
+//
+//  Created by Yeonji Baek on 7/19/21.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo]{
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        
+        return[swift, dog]
+    }
+    
+    
+    
+    
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        }
+        else{
+            cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
